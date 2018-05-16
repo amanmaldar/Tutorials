ea1952faaf3999435c390e4ce4c4cece467241bb
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-05-16 14:55:20 -0400

    Update firstClassFunctions.py

--- firstClassFunctions.py
+++ firstClassFunctions.py
@@ -3,31 +3,42 @@
 def adder(number):
     return number + number;
 
-def func_square(arg_list):
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+def simple_square(arg_list):
     result = []
     for i in arg_list:
         result.append(i*i)
     return  result;
 
 
-def func_square_first_class(func, arg_list):
+def mapping_first_class(func, arg_list):
     result = []
     for i in arg_list:
-        result.append(adder(i))
+        result.append(func(i))  # func variable is passing during function call
 
     return result
 
 
 numbers = [1,2,3,4,5]
-print(numbers)
+print "numbers: ", numbers
 
 # This is simple call to the function func_square
-result = func_square(numbers)
-print "Simple call: " , result
+result = simple_square(numbers)
+print "Simple square call: " , result
 
 # Following line illustartes concept of first class functions.  We call function func_square. Along with that we also tell the fucntion
 # which fucntion to call, here we are calling "adder". We also pass the arguments to the fucntion as arg_list.
-result = func_square_first_class(adder, numbers)
+result = mapping_first_class(adder, numbers)    # just pass the fucntion name
 print "Calling the adder function from function: ", result
 
+result = mapping_first_class(square, numbers)
+print "Calling the square function from function: ", result
+
+result = mapping_first_class(cube, numbers)
+print "Calling the cube function from function: ", result
 
