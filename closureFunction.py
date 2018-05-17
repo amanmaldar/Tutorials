971b6b730465e906b739fdb123972d5408c52d6a
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-05-16 20:17:55 -0400

    Create closureFunction.py

---
+++ closureFunction.py
@@ -0,0 +1,26 @@
+# closure functions and variables.
+# In simple terms a closure is an inner fucntion that rememebr and has access to variables in local scope in which it (inner fucntion)
+# has been created, even when outer function has finished executing.
+
+def add(x,y):
+    return x+y
+
+def outer_func(msg):
+    message = msg
+    def inner_func():
+        print message
+
+    return inner_func
+
+hi_func = outer_func("Hi")
+hello_func = outer_func("Hello")
+
+# these functions actually remember the values previous passed to outer functions.
+# so when we call a function by putting brackets () next to the pointer. The inner_func() gets called
+# and it prints previous values
+hi_func() 
+hello_func()
+
+#output
+#Hi
+#Hello
