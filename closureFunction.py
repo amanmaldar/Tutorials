c571f647d923a13229c53a3ddbc62a1c679dbaa6
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-05-18 18:48:20 -0400

    Update closureFunction.py

--- closureFunction.py
+++ closureFunction.py
@@ -27,3 +27,25 @@ hello_func()
 #output
 #Hi
 #Hello
+
+
+'''
+# removing the middle man
+
+def outer_func(msg):
+    # message = msg # removed this
+    def inner_func(msg):
+        print message
+
+    return inner_func
+
+hi_func = outer_func("Hi")
+hello_func = outer_func("Hello")
+
+hi_func() 
+hello_func()
+
+#output
+#Hi
+#Hello
+'''
